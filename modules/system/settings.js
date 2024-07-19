6c74cd9dbe77b0cf3f492c29dfebfe801a075f96
YanK l'Innommé <yanklinnomme@gmail.com>
2024-07-19 15:38:33 +0200

    MàJ du debug mode et ajout d'un mode basse résolution

--- modules/system/settings.js
+++ modules/system/settings.js
@@ -15,5 +15,59 @@ export const registerSystemSettings = () => {
     }
   };
 
+  const lowSpecSetting = {
+    name: game.i18n.localize("k4lt.Settings.LowSpec"),
+    hint: game.i18n.localize("k4lt.Settings.LowSpecHint"),
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean,
+    onChange: value => {
+      applyLowSpecCSS(value);
+    }
+  };
+
   game.settings.register("k4lt", "debug", debugSetting);
+  game.settings.register("k4lt", "lowSpec", lowSpecSetting);
+
+  // Appliquer le style au chargement initial
+  const isLowSpec = game.settings.get("k4lt", "lowSpec");
+  applyLowSpecCSS(isLowSpec);
+};
+
+const getAssetPath = (filename) => {
+  return `systems/k4lt/assets/${filename}`;
+};
+
+const applyLowSpecCSS = (isLowSpec) => {
+  const css = isLowSpec ? `
+    .kult-sheet {
+      background: url(${getAssetPath('background-sheet.webp')});
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+      padding: 10px;
+      color: rgb(32, 32, 32);
+      border: none;
+    }
+  ` : `
+    .kult-sheet {
+      background: url(${getAssetPath('background-sheet.svg')});
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+      padding: 10px;
+      color: rgb(32, 32, 32);
+      border: none;
+    }
+  `;
+  addCSS(css, 'low-spec-style');
+};
+
+const addCSS = (css, id) => {
+  let style = document.getElementById(id);
+  if (!style) {
+    style = document.createElement('style');
+    style.id = id;
+    document.head.appendChild(style);
+  }
+  style.innerHTML = css;
 };
